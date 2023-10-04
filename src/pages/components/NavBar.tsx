18eb34f533e4d07e9707354ca85979b9e5908e81
Vincent Demaisip <demaisipvincent@gmail.com>
2023-10-03 20:24:59 -0400

    Import Sidebar Icons

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -1,1 +1,10 @@
 //Nav bar on the left side
+import { BsPeople } from 'react-icons/bs';
+import { MdEventNote } from 'react-icons/md';
+import { BsListTask } from 'react-icons/bs';
+import { BsSuitClubFill } from 'react-icons/bs';
+
+export default function NavBar() {
+    
+
+}
