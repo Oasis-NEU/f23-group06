84c72c46e63f111657e2f87b918231cf458144ee
Vincent Demaisip <demaisipvincent@gmail.com>
2023-10-04 22:13:08 -0400

    Created NavBar
    Created NavBar itself and the Members, Events, and Task sections

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -4,7 +4,59 @@ import { MdEventNote } from 'react-icons/md';
 import { BsListTask } from 'react-icons/bs';
 import { BsSuitClubFill } from 'react-icons/bs';
 
-export default function NavBar() {
-    
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
-}
+const drawerWidth = 240;
+
+export default function NavBar() {
+    return(
+        <Box sx={{display: 'flex'}}>
+        <Drawer
+            sx={{
+            width: 290,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+            },
+            }}
+            variant="permanent"
+            anchor="left"
+        >
+            <Toolbar />
+                <Divider />
+            <List>
+                <Typography variant = 'h4'>
+                    <ListItemButton >
+                        <ListItemIcon>
+                            <BsPeople />
+                        </ListItemIcon>
+                        <ListItemText primary="Members" />
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <MdEventNote />
+                        </ListItemIcon>
+                        <ListItemText primary="  Events" />
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <BsListTask />
+                        </ListItemIcon>
+                        <ListItemText primary="  Tasks" />
+                    </ListItemButton>
+                </Typography>
+            </List>
+        </Drawer>
+        </Box>
+    );
+} 
