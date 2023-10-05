4fcb7ec2ed754292527d10c0157b219b0b6f3a05
Russell Leung <russell-leung@comcast.net>
2023-10-05 10:44:23 -0400

    Create List Members Page

--- src/pages/club/members/add.tsx
+++ src/pages/club/members/add.tsx
@@ -1,9 +1,10 @@
+import MemberModal from "../../components/MemberModal"
+
 export default function Page() {
     return (
         <div className="w-full h-screen">
             <div className="mx-auto w-min mt-1/2 h-min">
-                <h1>List Members</h1>
-                {/* Add Table & button */}
+                <MemberModal/>
             </div>
         </div>
     )
