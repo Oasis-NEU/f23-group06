59934c15dc0e22ece6a0d8b16dd21c1d0994a300
Vincent Demaisip <demaisipvincent@gmail.com>
2023-10-04 22:13:57 -0400

    Removed Unnecessary Icon

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -2,7 +2,6 @@
 import { BsPeople } from 'react-icons/bs';
 import { MdEventNote } from 'react-icons/md';
 import { BsListTask } from 'react-icons/bs';
-import { BsSuitClubFill } from 'react-icons/bs';
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
