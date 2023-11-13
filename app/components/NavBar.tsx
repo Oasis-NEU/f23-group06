2961afcd017444f4b45fcbac50850bce0927c191
russell-leung <russell-leung@comcast.net>
2023-11-13 12:28:11 -0500

    Fix Nav Bar Again

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -7,7 +7,6 @@ import logo from '../../public/ClubHub-logos_transparent.svg'
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
@@ -52,45 +51,48 @@ export default function NavBar() {
     };
 
     return(
-    <Box sx={{display: 'flex'}}>
-    <Drawer
-        sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-        },
-        }}
-        variant="permanent"
-        anchor="left"
-    >
-        <Box sx={{display: 'flex', justifyContent: 'center'}}>
-            <Link href="http://localhost:3000/">
-                <Image
-                        src={logo}
-                        alt='Logo'
-                        width={200}
-                        height={200}
-                />
-            </Link>
-        </Box>
-        <Divider />
-        <List>
-            {navItems.map((item, index) => (
-                <Typography variant = 'h4' key={index}>
-                    <Link href={item.link} style={{ textDecoration: 'none'}}>
-                        <ListItemButton selected={selectedIndex === index && index != 0} onClick={(event) => handleListItemClick(event, index)}>
-                            <ListItemIcon>
-                                {item.icon}
-                            </ListItemIcon>
-                            <ListItemText className="text-black " primary={item.label} />
-                        </ListItemButton>
+        <>
+            <Box sx={{display: 'flex'}}>
+            <Drawer
+                sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: drawerWidth,
+                    boxSizing: 'border-box',
+                },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Box sx={{display: 'flex', justifyContent: 'center'}}>
+                    <Link href="..">
+                        <Image
+                                src={logo}
+                                priority={false}
+                                alt='Logo'
+                                width={200}
+                                height={200}
+                        />
                     </Link>
-                </Typography>
-            ))}
-        </List>
-    </Drawer>
-    </Box>
+                </Box>
+                <Divider />
+                <List>
+                    {navItems.map((item, index) => (
+                        <Typography variant = 'h4' key={index}>
+                            <Link href={item.link} style={{ textDecoration: 'none'}}>
+                                <ListItemButton selected={selectedIndex === index && index != 0} onClick={(event) => handleListItemClick(event, index)}>
+                                    <ListItemIcon>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText className="text-black " primary={item.label} />
+                                </ListItemButton>
+                            </Link>
+                        </Typography>
+                    ))}
+                </List>
+            </Drawer>
+            </Box>
+        </ >
     );
 }
