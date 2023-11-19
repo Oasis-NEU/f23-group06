aea440c4da6230536334474eaf52c48b0debc08a
russell-leung <russell-leung@comcast.net>
2023-11-19 13:10:07 -0500

    remove usless code

--- app/components/ClubButton.tsx
+++ app/components/ClubButton.tsx
@@ -2,13 +2,9 @@
 import { Button, Link } from "@nextui-org/react";
 
 export function ClubButton({clubId}: any) {
-    const saveClubId = () => {
-        localStorage.setItem("clubId", clubId.toString());
-    };
-
   return (
     <Link href={`/${clubId}`}>
-      <Button variant="bordered" color="primary" onClick={saveClubId}>
+      <Button variant="bordered" color="primary">
         Go to Club
       </Button>
     </Link>
